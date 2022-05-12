f4e98fe21be1f32da6fa72a50d03bb3e24b91200
luosh18 <luosh18@mails.tsinghua.edu.cn>
2022-05-12 20:27:15 +0800

    remove old dataset generation

--- workspace/scripts/gen_vanilla.sh
+++
@@ -1,6 +0,0 @@
-python3 jaco_dataset_generator.py \
-    --save_path=$HOME/disk/rlbench_data/ --tasks=pick_and_place \
-    --processes=8 --episodes_per_task=10
-python3 preprocess_dataset.py \
-    --save_path=$HOME/disk/preprocessed_data/ \
-    --data_path=$HOME/disk/rlbench_data/ 
