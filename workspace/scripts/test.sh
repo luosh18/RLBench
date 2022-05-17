4b20e9c33fb6feddfdacfee677980e614aa0f118
luosh18 <luosh18@mails.tsinghua.edu.cn>
2022-05-17 15:41:33 +0800

    fix scripts

--- workspace/scripts/test.sh
+++ workspace/scripts/test.sh
@@ -1,7 +1,7 @@
 python3 learn/test.py \
-    --save_dir=$HOME/disk/train_noMDN \
-    --dataset_root=$HOME/disk/dataset \
+    --save_dir=$HOME/disk/train_ee \
+    --dataset_root=$HOME/disk/dataset_ee \
     --task_name=pick_and_place_test \
     --T=40 --simulation_timestep=0.1 \
     --state_size=3 --action_size=4 \
-    --iteration=33500
+    --iteration=75000
--- workspace/scripts/train.sh
+++ workspace/scripts/train.sh
@@ -1,5 +1,5 @@
 python3 learn/train.py \
-    --save_dir=$HOME/disk/train_noMDN \
+    --save_dir=$HOME/disk/train_ee \
     --dataset_root=$HOME/disk/dataset_ee \
     --task_name=pick_and_place \
     --T=40 --simulation_timestep=0.1 \
