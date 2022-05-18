93cc8ecd11d0229c230979186ea21cfd68b13273
luosh18 <luosh18@mails.tsinghua.edu.cn>
2022-05-18 19:23:11 +0800

    scripts

--- workspace/scripts/gripper_train.sh
+++ workspace/scripts/gripper_train.sh
@@ -1,6 +1,7 @@
 python3 learn/train.py \
-    --save_dir=$HOME/disk/train_noMDN \
+    --save_dir=$HOME/disk/train_noMDN_gripper \
     --dataset_root=$HOME/disk/dataset_new \
     --task_name=pick_and_place \
     --T=40 --simulation_timestep=0.1 \
-    --state_size=9 --action_size=7
+    --state_size=9 --action_size=7 \
+    --gripper_action=True
--- workspace/scripts/test.sh
+++ workspace/scripts/test.sh
@@ -1,6 +1,6 @@
 python3 learn/test.py \
     --save_dir=$HOME/disk/train_noMDN \
-    --dataset_root=$HOME/disk/dataset \
+    --dataset_root=$HOME/disk/dataset_new \
     --task_name=pick_and_place_test \
     --T=40 --simulation_timestep=0.1 \
     --state_size=9 --action_size=7 \
