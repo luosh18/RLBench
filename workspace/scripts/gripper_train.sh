53a75ba27f221406c6f6bce1099d64f663354850
luosh18 <luosh18@mails.tsinghua.edu.cn>
2022-05-19 18:59:13 +0800

    fix scripts

--- workspace/scripts/gripper_train.sh
+++ workspace/scripts/gripper_train.sh
@@ -1,6 +1,7 @@
 python3 learn/train.py \
-    --save_dir=$HOME/disk/train_ee \
+    --save_dir=$HOME/disk/train_ee_gripper \
     --dataset_root=$HOME/disk/dataset_new \
     --task_name=pick_and_place \
     --T=40 --simulation_timestep=0.1 \
-    --state_size=3 --action_size=4
+    --state_size=3 --action_size=4 \
+    --gripper_action=True
--- workspace/scripts/test.sh
+++ workspace/scripts/test.sh
@@ -1,6 +1,6 @@
 python3 learn/test.py \
-    --save_dir=$HOME/disk/train_ee \
-    --dataset_root=$HOME/disk/dataset_ee \
+    --save_dir=$HOME/disk/train_new \
+    --dataset_root=$HOME/disk/dataset_new \
     --task_name=pick_and_place_test \
     --T=40 --simulation_timestep=0.1 \
     --state_size=3 --action_size=4 \
