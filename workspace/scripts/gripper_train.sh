216dd32d6abd68a93dc7da27878bbde73fffa75a
luosh18 <luosh18@mails.tsinghua.edu.cn>
2022-05-24 09:41:05 +0800

    script con

--- workspace/scripts/gripper_train.sh
+++ workspace/scripts/gripper_train.sh
@@ -1,6 +1,6 @@
 python3 learn/train.py \
     --save_dir=$HOME/disk/train_noMDN_gripper \
-    --dataset_root=$HOME/disk/dataset_new \
+    --dataset_root=$HOME/disk/dataset_con \
     --task_name=pick_and_place \
     --T=40 --simulation_timestep=0.1 \
     --state_size=9 --action_size=7 \
